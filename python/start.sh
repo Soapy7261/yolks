421214d429b9384e023001e6baac06d015fd6d9a
Soapy7261 <81933267+Soapy7261@users.noreply.github.com>
2024-07-26 23:01:42 -0400

    no more start.sh

--- python/start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/ash
-echo "Upgrading pip..."
-pip install -q --user --upgrade pip
-echo "Installing requirements..."
-pip install -q --user -r requirements.txt
-echo "Running python!"
-python main.py
-echo "Exiting..."
