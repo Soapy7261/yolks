f209d4ecb86f1e61a3b5f1fd3c2dfca084a411d3
Soapy7261 <81933267+Soapy7261@users.noreply.github.com>
2024-07-26 22:57:20 -0400

    maybe we dont need start.sh anymore!

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -2,11 +2,16 @@
 #sleep 2 lets see why this is needed!
 
 cd /home/container
-MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')`
+#MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g')` dont need this
 
 # Make internal Docker IP address available to processes.
 export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'`
 
 # Run the Server
-echo -e ":/home/container$ ${MODIFIED_STARTUP}"
-/start.sh "${MODIFIED_STARTUP}"
+echo "Upgrading pip..."
+pip install -q --user --upgrade pip
+echo "Installing requirements..."
+pip install -q --user -r requirements.txt
+echo "Running python!"
+python main.py
+echo "Exiting..."
