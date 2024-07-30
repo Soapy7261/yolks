92a219c32ab75e30032273260b6c8df3f7963221
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-07-29 23:36:24 -0400

    more checks, and actually export

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -3,10 +3,11 @@
 TZ=${TZ:-UTC}
 export TZ
 
-cd /home/container
+cd /home/container || exit 1
 
 # Make internal Docker IP address available to processes.
-export INTERNAL_IP=`ip route get 1 | awk '{print $NF;exit}'` # you're on the chopping block buddy
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP # Don't know why this would be needed but okay ig :shrug:
 
 # Run the Program
 echo "Upgrading pip..."
