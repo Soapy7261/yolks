8d4a9f23279f168a23048d97a7c91b3753a57094
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-09-16 04:07:36 -0400

    lets see how fast this breaks

--- java/entrypoint-thanos.sh
+++ java/entrypoint-thanos.sh
@@ -27,9 +27,9 @@ TZ=${TZ:-UTC}
 export TZ
 
 # Set environment variable that holds the Internal Docker IP
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP #I dont install those packages for thanos anyway.
-
+#INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+#export INTERNAL_IP #I dont install those packages for thanos anyway.
+#Lets see how fast this breaks :P
 # Switch to the container's working directory
 cd /home/container || exit 1
 
