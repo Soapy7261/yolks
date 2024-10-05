7fa51be75540cb16d1e4cee608b5acd65c34bdba
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-10-05 01:55:30 -0400

    check if it exists and also only install if its been changed

--- python/entrypoint.sh
+++ python/entrypoint.sh
@@ -13,7 +13,15 @@ export INTERNAL_IP # Don't know why this would be needed but okay ig :shrug:
 echo "Upgrading pip..."
 pip install --user --upgrade pip
 echo "Installing requirements..."
-pip install --user -r requirements.txt -U
+if [[ -f "./requirements.txt" ]]; then
+    file_mod_time=$(stat -c %y ./requirements.txt)
+    last_boot=$(who -b | awk '{print $3 " " $4}')
+    if [[ $(date -d "$file_mod_time" +%s) -gt $(date -d "$last_boot" +%s) ]]; then
+        pip install --user -r requirements.txt -U
+    fi
+else
+    echo "No requirements.txt found, not installing any dependencies!"
+fi
 echo "Running main.py!"
 python main.py
 echo "Exiting..."
