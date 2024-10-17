730b10b36b3d6245f479625cacf51a8dca10d83c
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-10-16 21:33:19 -0400

    a few things
    removes unsupported versions of node, changes how not used stuff is stored, modify eggs, modify entrypoints for python

--- abandoned/old_python_entrypoint.sh
+++ abandoned/old_python_entrypoint.sh
@@ -14,10 +14,20 @@ echo "Upgrading pip..."
 pip install --user --upgrade pip
 echo "Installing requirements..."
 if [[ -f "./requirements.txt" ]]; then
-    pip install --user -r requirements.txt -U
+    file_mod_time=$(stat "./requirements.txt" | grep 'Modify' | awk '{print $2 " " substr($3,1,8)}')
+    last_boot=$(cat /proc/uptime | awk '{print $1}')
+    boot_time=$(date -d "@$(($(date +%s) - ${last_boot%.*}))" +"%Y-%m-%d %H:%M:%S")
+    if [ "$(date -d "$file_mod_time" +%s)" -gt "$(date -d "$boot_time" +%s)" ]; then
+        pip install --user -r requirements.txt -U
+    else
+        echo "Requirements have not changed."
+    fi
 else
     echo "No requirements.txt found, not installing any dependencies!"
 fi
+unset file_mod_time
+unset last_boot
+unset boot_time
 echo "Running script..."
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 env ${PARSED}
