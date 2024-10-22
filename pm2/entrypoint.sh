7fcb5f6c6427397175cb5fffef167b622f03c13e
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-10-21 21:17:17 -0400

    reinstall npm and pm2 i guess

--- pm2/entrypoint.sh
+++ pm2/entrypoint.sh
@@ -10,12 +10,22 @@ INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP # Don't know why this would be needed but okay ig :shrug:
 
 # Run the Program
-echo "Upgrading NPM..."
-npm install npm@latest
-echo "Upgrading PM2..."
-npm install pm2@latest
-echo "Updating PM2..."
-pm2 update
+#echo "Upgrading NPM..."
+#npm install npm@latest
+#echo "Upgrading PM2..."
+#npm install pm2@latest
+#echo "Updating PM2..."
+#pm2 update
+if [[ -f "./package.json" ]]; then
+    echo "Installing packages from package.json..."
+    npm i
+else
+    echo "No package.json found, not installing any packages!"
+fi
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mnode -v\n"
+node -v
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mpm2 -v\n"
+pm2 -v
 echo "Running script..."
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 env ${PARSED}
