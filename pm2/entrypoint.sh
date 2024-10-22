a84519b1714eb86b23ef74a7b14d4de6fd8c12f9
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-10-21 21:18:56 -0400

    reorder

--- pm2/entrypoint.sh
+++ pm2/entrypoint.sh
@@ -16,16 +16,16 @@ export INTERNAL_IP # Don't know why this would be needed but okay ig :shrug:
 #npm install pm2@latest
 #echo "Updating PM2..."
 #pm2 update
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mnode -v\n"
+node -v
+printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mpm2 -v\n"
+pm2 -v
 if [[ -f "./package.json" ]]; then
     echo "Installing packages from package.json..."
     npm i
 else
     echo "No package.json found, not installing any packages!"
 fi
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mnode -v\n"
-node -v
-printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0mpm2 -v\n"
-pm2 -v
 echo "Running script..."
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 env ${PARSED}
