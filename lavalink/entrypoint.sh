8dcd5172d98083cf9876775080e9f94e0e220a6e
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-11-02 03:03:55 -0400

    i swear i actually will tomorrow

--- lavalink/entrypoint.sh
+++ lavalink/entrypoint.sh
@@ -9,6 +9,17 @@ cd /home/container || exit 1
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP # Don't know why this would be needed but okay ig :shrug:
 
+echo "Updating Lavalink..."
+
+DOWNLOAD_URL=$(curl -s "https://api.github.com/repos/lavalink-devs/Lavalink/releases/latest" | grep "browser_download_url" | grep "jar" | cut -d '"' -f 4)
+
+if [ -z "$DOWNLOAD_URL" ]; then
+    echo "No .jar file found in the latest release."
+    exit 1
+fi
+
+curl -L -o ./lavalink.jar "$DOWNLOAD_URL"
+
 echo "Running Lavalink..."
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 
