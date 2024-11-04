d5178b38c0cf38d6524c1119e11dba3bc3a840e0
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-11-04 16:15:26 -0500

    Update entrypoint.sh

--- mongodb/entrypoint.sh
+++ mongodb/entrypoint.sh
@@ -6,12 +6,17 @@ export TZ
 cd /home/container || exit 1
 
 # Make internal Docker IP address available to processes.
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP # Don't know why this would be needed but okay ig :shrug:
+if command -v ip >/dev/null 2>&1; then
+    INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+    export INTERNAL_IP
+else
+    echo 'IP not installed, skipping setting internal IP... (This should be ignored.)'
+fi
 
 # Run the Program
 if [ ! -d "./mongodb-data" ]; then
     mkdir /home/container/mongodb-data
+fi
 
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 env ${PARSED}
