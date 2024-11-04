8aed5d1027fcde9129aec9837d9860fafff1ee71
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-11-04 16:40:33 -0500

    oh so THATS the problem, i KNEW it

--- lavalink/latest/entrypoint.sh
+++ lavalink/latest/entrypoint.sh
@@ -29,5 +29,4 @@ PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat
 
 printf "\033[1m\033[33mcontainer@pterodactyl~ \033[0m%s\n" "$PARSED"
 
-env ${PARSED}
-echo "Exiting..."
+exec env ${PARSED}
