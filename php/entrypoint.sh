1d4e0534036564af4fb669a7c5481827f3af6675
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-11-19 17:12:40 -0500

    caught it before it happened

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -9,15 +9,6 @@ cd /home/container || exit 1
 INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
 export INTERNAL_IP # Don't know why this would be needed but okay ig :shrug:
 
-# Run the Program
-echo "Upgrading pip..."
-pip install --user --upgrade pip
-echo "Installing requirements..."
-if [[ -f "./requirements.txt" ]]; then
-    pip install --user -r requirements.txt -U
-else
-    echo "No requirements.txt found, not installing any dependencies!"
-fi
 echo "Running script..."
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
 exec env ${PARSED}
