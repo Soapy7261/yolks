6e20846af67920f04a2e86ba7807a2a494958512
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-11-19 18:09:37 -0500

    fix

--- php/entrypoint.sh
+++ php/entrypoint.sh
@@ -6,8 +6,12 @@ export TZ
 cd /home/container || exit 1
 
 # Make internal Docker IP address available to processes.
-INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
-export INTERNAL_IP # Don't know why this would be needed but okay ig :shrug:
+if command -v ip >/dev/null 2>&1; then
+    INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+    export INTERNAL_IP
+else
+    echo 'IP not installed, skipping setting internal IP... (This should be ignored.)'
+fi
 
 echo "Running script..."
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
