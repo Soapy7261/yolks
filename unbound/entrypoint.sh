7a3478ab7fff96b1b292a91ea2440c1c94f8f242
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-11-21 02:09:36 -0500

    there is 0 chance that this works, but on the off chance it does, i will watch the entirety of skibidi toilet

---
+++ unbound/entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+TZ=${TZ:-UTC}
+export TZ
+
+cd /home/container || exit 1
+
+# Make internal Docker IP address available to processes.
+INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+export INTERNAL_IP # Don't know why this would be needed but okay ig :shrug:
+
+# Create the SSL directory if it doesn't exist
+mkdir -p /home/container/ssl
+
+KEY_FILE="/home/container/ssl/private.key"
+CERT_FILE="/home/container/ssl/certificate.pem"
+
+# Run the Program
+echo "Generating 4096-bit RSA TLS key and certificate..."
+openssl req -x509 -newkey rsa:4096 -nodes -keyout $KEY_FILE -out $CERT_FILE -days 365 \
+    -subj "/CN=${ADDRESS}"
+echo "TLS key and certificate generated with 4096-bit RSA."
+
+if [ ! -f "./unbound.conf" ]; then
+    echo "No unbound.conf found, downloading default..."
+    curl -sL -o ./unbound.conf https://raw.githubusercontent.com/Soapy7261/yolks/refs/heads/master/unbound/defaultconfig.txt
+fi
+
+echo "Running script..."
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+exec unbound -d -c /home/container/unbound.conf
+#echo "Exiting..."
