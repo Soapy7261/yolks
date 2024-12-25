a7611fa4203162ee4f2ce561b78d71d9441c2343
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2024-12-25 17:10:59 -0500

    Update entrypoint-mr.sh

--- python/entrypoint-mr.sh
+++ python/entrypoint-mr.sh
@@ -72,8 +72,8 @@ if [ "$PARFC" == "1" ]; then
     #Check for new changes every 1 minute, and if there are any, restart the script by just entirely restarting the container.
     while true; do
         sleep 60
-        OUTPUT=$(git -C /home/container/scripts pull "$GIT_REPO" 2>/dev/null)
-        if echo "$OUTPUT" | grep -q "Already up to date."; then
+        # OUTPUT=$(git -C /home/container/scripts pull "$GIT_REPO" 2>/dev/null)
+        if [ -z "$(git -C /home/container/scripts status -s)" ]; then
             #echo "No changes detected, continuing..."
             :
         else
