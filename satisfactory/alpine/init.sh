ddf47600e60ae44c871b73476d6d482dfa631ee7
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2025-01-13 01:51:02 -0500

    add debian, use O1 for alpine

--- satisfactory/alpine/init.sh
+++ satisfactory/alpine/init.sh
@@ -1,18 +1,20 @@
-#!/bin/ash
+#!/bin/sh
 set -e
 
-printf "===== Satisfactory Server %s =====\\nhttps://github.com/wolveix/satisfactory-server\\n\\n" "$VERSION"
+printf "===== Satisfactory Server %s =====\nhttps://github.com/wolveix/satisfactory-server\n\n" "$VERSION"
 
-CURRENTUID=$(id -u)
+CURRENTUID="$(id -u)"
 HOME="/home/container"
 MSGERROR="\033[0;31mERROR:\033[0m"
 MSGWARNING="\033[0;33mWARNING:\033[0m"
 NUMCHECK='^[0-9]+$'
-RAMAVAILABLE=$(awk '/MemAvailable/ {printf( "%d\n", $2 / 1024000 )}' /proc/meminfo)
+RAMAVAILABLE="$(awk '/MemAvailable/ {printf( "%d\n", $2 / 1024000 )}' /proc/meminfo)"
 USER="container"
 
-if [[ "${DEBUG,,}" == "true" ]]; then
-    printf "Debugging enabled (the container will exit after printing the debug info)\\n\\nPrinting environment variables:\\n"
+# Convert DEBUG to lowercase for comparison
+DEBUG_LOWER="$(echo "$DEBUG" | tr '[:upper:]' '[:lower:]')"
+if [ "$DEBUG_LOWER" = "true" ]; then
+    printf "Debugging enabled (the container will exit after printing the debug info)\n\nPrinting environment variables:\n"
     export
 
     echo "
@@ -20,75 +22,93 @@ System info:
 OS:  $(uname -a)
 CPU: $(lscpu | grep '^Model name:' | sed 's/Model name:[[:space:]]*//g')
 RAM: $(awk '/MemAvailable/ {printf( "%d\n", $2 / 1024000 )}' /proc/meminfo)GB/$(awk '/MemTotal/ {printf( "%d\n", $2 / 1024000 )}' /proc/meminfo)GB
-HDD: $(df -h | awk '$NF=="/"{printf "%dGB/%dGB (%s used)\n", $3,$2,$5}')"
-    printf "\\nCurrent version:\\n%s" "${VERSION}"
-    printf "\\nCurrent user:\\n%s" "$(id)"
-    printf "\\nProposed user:\\nuid=%s(?) gid=%s(?) groups=%s(?)\\n" "$PUID" "$PGID" "$PGID"
-    printf "\\nExiting...\\n"
+HDD: $(df -h | awk '\$NF=="/"{printf "%dGB/%dGB (%s used)\n", $3,$2,$5}')"
+    printf "\nCurrent version:\n%s" "${VERSION}"
+    printf "\nCurrent user:\n%s" "$(id)"
+    printf "\nProposed user:\nuid=%s(?) gid=%s(?) groups=%s(?)\n" "$PUID" "$PGID" "$PGID"
+    printf "\nExiting...\n"
     exit 1
 fi
 
-# check that the cpu isn't generic, as Satisfactory will normally crash
-if [[ "$VMOVERRIDE" == "true" ]]; then
-    printf "${MSGWARNING} VMOVERRIDE is enabled, skipping CPU model check. Satisfactory might crash!\\n"
+# Check CPU model to avoid crashing on generic KVM/QEMU
+VMOVERRIDE_LOWER="$(echo "$VMOVERRIDE" | tr '[:upper:]' '[:lower:]')"
+if [ "$VMOVERRIDE_LOWER" = "true" ]; then
+    printf "${MSGWARNING} VMOVERRIDE is enabled, skipping CPU model check. Satisfactory might crash!\n"
 else
-    cpu_model=$(lscpu | grep 'Model name:' | sed 's/Model name:[[:space:]]*//g')
-    if [[ "$cpu_model" == "Common KVM processor" || "$cpu_model" == *"QEMU"* ]]; then
-        printf "${MSGERROR} Your CPU model is configured as \"${cpu_model}\", which will cause Satisfactory to crash.\\nIf you have control over your hypervisor (ESXi, Proxmox, etc.), you should be able to easily change this.\\nOtherwise contact your host/administrator for assistance.\\n"
-        exit 1
-    fi
+    cpu_model="$(lscpu | grep 'Model name:' | sed 's/Model name:[[:space:]]*//g')"
+    # If the CPU model is "Common KVM processor" or contains "QEMU"
+    case "$cpu_model" in
+        "Common KVM processor"|*"QEMU"*)
+            printf "${MSGERROR} Your CPU model is \"${cpu_model}\", which will cause Satisfactory to crash.\n"
+            printf "If you have control over your hypervisor (ESXi, Proxmox, etc.), you should be able to easily change this.\n"
+            printf "Otherwise contact your host/administrator for assistance.\n"
+            exit 1
+            ;;
+    esac
 fi
 
-printf "Checking available memory: %sGB detected\\n" "$RAMAVAILABLE"
-if [[ "$RAMAVAILABLE" -lt 8 ]]; then
-    printf "${MSGWARNING} You have less than the required 8GB minimum (%sGB detected) of available RAM to run the game server.\\nThe server will likely run fine, though may run into issues in the late game (or with 4+ players).\\n" "$RAMAVAILABLE"
+printf "Checking available memory: %sGB detected\n" "$RAMAVAILABLE"
+if [ "$RAMAVAILABLE" -lt 8 ]; then
+    printf "${MSGWARNING} You have less than the required 8GB minimum (%sGB detected) of available RAM to run the game server.\n" "$RAMAVAILABLE"
+    printf "The server will likely run fine, though may run into issues in the late game (or with 4+ players).\n"
 fi
 
-# prevent large logs from accumulating by default
-if [[ "${LOG,,}" != "true" ]]; then
-    printf "Clearing old Satisfactory logs (set LOG=true to disable this)\\n"
-    if [ -d "/home/container/config/gamefiles/FactoryGame/Saved/Logs" ] && [ -n "$(find /home/container/config/gamefiles/FactoryGame/Saved/Logs -type f -print -quit)" ]; then
+# Clear old logs by default if LOG != true
+LOG_LOWER="$(echo "$LOG" | tr '[:upper:]' '[:lower:]')"
+if [ "$LOG_LOWER" != "true" ]; then
+    printf "Clearing old Satisfactory logs (set LOG=true to disable this)\n"
+    if [ -d "/home/container/config/gamefiles/FactoryGame/Saved/Logs" ] && \
+       [ -n "$(find /home/container/config/gamefiles/FactoryGame/Saved/Logs -type f -print -quit)" ]; then
         rm -r /home/container/config/gamefiles/FactoryGame/Saved/Logs/* || true
     fi
 fi
 
-# check if the user and group IDs have been set
-if [[ "$CURRENTUID" -ne "0" ]] && [[ "${ROOTLESS,,}" != "true" ]]; then
-    printf "${MSGERROR} Current user (%s) is not root (0)\\nPass your user and group to the container using the PGID and PUID environment variables\\nDo not use the --user flag (or user: field in Docker Compose) without setting ROOTLESS=true\\n" "$CURRENTUID"
+ROOTLESS_LOWER="$(echo "$ROOTLESS" | tr '[:upper:]' '[:lower:]')"
+# Check user ID and group ID requirements
+if [ "$CURRENTUID" -ne 0 ] && [ "$ROOTLESS_LOWER" != "true" ]; then
+    printf "${MSGERROR} Current user (%s) is not root (0)\n" "$CURRENTUID"
+    printf "Pass your user and group to the container using the PGID and PUID environment variables\n"
+    printf "Do not use the --user flag (or user: field in Docker Compose) without setting ROOTLESS=true\n"
     exit 1
 fi
 
-if ! [[ "$PGID" =~ $NUMCHECK ]] ; then
-    printf "${MSGWARNING} Invalid group id given: %s\\n" "$PGID"
+# Validate PGID
+if ! echo "$PGID" | grep -Eq '^[0-9]+$'; then
+    printf "${MSGWARNING} Invalid group id given: %s\n" "$PGID"
     PGID="1000"
-elif [[ "$PGID" -eq 0 ]]; then
-    printf "${MSGERROR} PGID/group cannot be 0 (root)\\n"
+elif [ "$PGID" -eq 0 ]; then
+    printf "${MSGERROR} PGID/group cannot be 0 (root)\n"
     exit 1
 fi
 
-if ! [[ "$PUID" =~ $NUMCHECK ]] ; then
-    printf "${MSGWARNING} Invalid user id given: %s\\n" "$PUID"
+# Validate PUID
+if ! echo "$PUID" | grep -Eq '^[0-9]+$'; then
+    printf "${MSGWARNING} Invalid user id given: %s\n" "$PUID"
     PUID="1000"
-elif [[ "$PUID" -eq 0 ]]; then
-    printf "${MSGERROR} PUID/user cannot be 0 (root)\\n"
+elif [ "$PUID" -eq 0 ]; then
+    printf "${MSGERROR} PUID/user cannot be 0 (root)\n"
     exit 1
 fi
 
-if [[ "${ROOTLESS,,}" != "true" ]]; then
-  if [[ $(getent group $PGID | cut -d: -f1) ]]; then
-      usermod -a -G "$PGID" container
-  else
-      groupmod -g "$PGID" container
-  fi
+# Update ownership if not running rootlessly
+if [ "$ROOTLESS_LOWER" != "true" ]; then
+    # Attempt to modify group
+    if [ "$(getent group "$PGID" | cut -d: -f1)" != "" ]; then
+        usermod -a -G "$PGID" container 2>/dev/null || true
+    else
+        groupmod -g "$PGID" container 2>/dev/null || true
+    fi
 
-  if [[ $(getent passwd ${PUID} | cut -d: -f1) ]]; then
-      USER=$(getent passwd $PUID | cut -d: -f1)
-  else
-      usermod -u "$PUID" container
-  fi
+    # Attempt to modify user
+    if [ "$(getent passwd "$PUID" | cut -d: -f1)" != "" ]; then
+        USER="$(getent passwd "$PUID" | cut -d: -f1)"
+    else
+        usermod -u "$PUID" container 2>/dev/null || true
+    fi
 fi
 
-if [[ ! -w "/home/container/config" ]]; then
+# Check write permissions
+if [ ! -w "/home/container/config" ]; then
     echo "The current user does not have write permissions for /home/container/config"
     exit 1
 fi
@@ -106,11 +126,13 @@ mkdir -p \
     /home/container/.steam/steam \
     || exit 1
 
-echo "Satisfactory logs can be found in home/container/config/gamefiles/FactoryGame/Saved/Logs" > /home/container/config/logs/satisfactory-path.txt
+echo "Satisfactory logs can be found in home/container/config/gamefiles/FactoryGame/Saved/Logs" \
+    > /home/container/config/logs/satisfactory-path.txt
 
-if [[ "${ROOTLESS,,}" != "true" ]]; then
-  chown -R "$PUID":"$PGID" /home/container/config /home/container /tmp/dumps
-  exec gosu "$USER" "/run.sh" "$@"
+if [ "$ROOTLESS_LOWER" != "true" ]; then
+    chown -R "$PUID":"$PGID" /home/container/config /home/container /tmp/dumps
+    # Replace 'gosu' with 'su-exec' or any other tool if not installed in your Alpine image
+    exec gosu "$USER" "/run.sh" "$@"
 else
-  exec "/run.sh" "$@"
+    exec "/run.sh" "$@"
 fi
--- satisfactory/alpine/run.sh
+++ satisfactory/alpine/run.sh
@@ -1,50 +1,77 @@
 #!/bin/ash
-
 set -e
 
+# Define your numeric check pattern here (since we can't do [[ =~ ]])
+NUMCHECK='^[0-9]+$'
+
+# Convert a variable to lowercase
+to_lower() {
+  echo "$1" | tr '[:upper:]' '[:lower:]'
+}
+
+# Validate numeric input with grep
+is_number() {
+  echo "$1" | grep -Eq "$NUMCHECK"
+}
+
+###############################################################################
 # Engine.ini settings
-if ! [[ "$AUTOSAVENUM" =~ $NUMCHECK ]]; then
-    printf "Invalid autosave number given: %s\\n" "$AUTOSAVENUM"
-    AUTOSAVENUM="5"
+###############################################################################
+if ! is_number "$AUTOSAVENUM"; then
+  printf "Invalid autosave number given: %s\\n" "$AUTOSAVENUM"
+  AUTOSAVENUM="5"
 fi
 printf "Setting autosave number to %s\\n" "$AUTOSAVENUM"
 
-if ! [[ "$MAXOBJECTS" =~ $NUMCHECK ]]; then
-    printf "Invalid max objects number given: %s\\n" "$MAXOBJECTS"
-    MAXOBJECTS="2162688"
+if ! is_number "$MAXOBJECTS"; then
+  printf "Invalid max objects number given: %s\\n" "$MAXOBJECTS"
+  MAXOBJECTS="2162688"
 fi
 printf "Setting max objects to %s\\n" "$MAXOBJECTS"
 
-if ! [[ "$MAXTICKRATE" =~ $NUMCHECK ]] ; then
-    printf "Invalid max tick rate number given: %s\\n" "$MAXTICKRATE"
-    MAXTICKRATE="120"
+if ! is_number "$MAXTICKRATE"; then
+  printf "Invalid max tick rate number given: %s\\n" "$MAXTICKRATE"
+  MAXTICKRATE="120"
 fi
 printf "Setting max tick rate to %s\\n" "$MAXTICKRATE"
 
-[[ "${SERVERSTREAMING,,}" == "true" ]] && SERVERSTREAMING="1" || SERVERSTREAMING="0"
+SERVERSTREAMING_LOWER="$(to_lower "$SERVERSTREAMING")"
+if [ "$SERVERSTREAMING_LOWER" = "true" ]; then
+  SERVERSTREAMING="1"
+else
+  SERVERSTREAMING="0"
+fi
 printf "Setting server streaming to %s\\n" "$SERVERSTREAMING"
 
-if ! [[ "$TIMEOUT" =~ $NUMCHECK ]] ; then
-    printf "Invalid timeout number given: %s\\n" "$TIMEOUT"
-    TIMEOUT="300"
+if ! is_number "$TIMEOUT"; then
+  printf "Invalid timeout number given: %s\\n" "$TIMEOUT"
+  TIMEOUT="300"
 fi
 printf "Setting timeout to %s\\n" "$TIMEOUT"
 
+###############################################################################
 # Game.ini settings
-if ! [[ "$MAXPLAYERS" =~ $NUMCHECK ]] ; then
-    printf "Invalid max players given: %s\\n" "$MAXPLAYERS"
-    MAXPLAYERS="4"
+###############################################################################
+if ! is_number "$MAXPLAYERS"; then
+  printf "Invalid max players given: %s\\n" "$MAXPLAYERS"
+  MAXPLAYERS="4"
 fi
 printf "Setting max players to %s\\n" "$MAXPLAYERS"
 
+###############################################################################
 # GameUserSettings.ini settings
-if [[ "${DISABLESEASONALEVENTS,,}" == "true" ]]; then
-    printf "Disabling seasonal events\\n"
-    DISABLESEASONALEVENTS="-DisableSeasonalEvents"
+###############################################################################
+DISABLESEASONALEVENTS_LOWER="$(to_lower "$DISABLESEASONALEVENTS")"
+if [ "$DISABLESEASONALEVENTS_LOWER" = "true" ]; then
+  printf "Disabling seasonal events\\n"
+  DISABLESEASONALEVENTS="-DisableSeasonalEvents"
 else
-    DISABLESEASONALEVENTS=""
+  DISABLESEASONALEVENTS=""
 fi
 
+###############################################################################
+# Build .ini arguments
+###############################################################################
 ini_args=(
   "-ini:Engine:[/Script/FactoryGame.FGSaveSession]:mNumRotatingAutosaves=$AUTOSAVENUM"
   "-ini:Engine:[/Script/Engine.GarbageCollectionSettings]:gc.MaxObjectsInEditor=$MAXOBJECTS"
@@ -60,61 +87,75 @@ ini_args=(
   "$DISABLESEASONALEVENTS"
 )
 
-if [[ "${SKIPUPDATE,,}" != "false" ]] && [ ! -f "/home/container/config/gamefiles/FactoryServer.sh" ]; then
-    printf "%s Skip update is set, but no game files exist. Updating anyway\\n" "${MSGWARNING}"
-    SKIPUPDATE="false"
+###############################################################################
+# Update game if needed
+###############################################################################
+SKIPUPDATE_LOWER="$(to_lower "$SKIPUPDATE")"
+if [ "$SKIPUPDATE_LOWER" != "false" ] && [ ! -f "/home/container/config/gamefiles/FactoryServer.sh" ]; then
+  # Warn user that no files exist to skip an update
+  printf "%s Skip update is set, but no game files exist. Updating anyway\\n" "$MSGWARNING"
+  SKIPUPDATE_LOWER="false"
 fi
 
-if [[ "${SKIPUPDATE,,}" != "true" ]]; then
-    if [[ "${STEAMBETA,,}" == "true" ]]; then
-        printf "Experimental flag is set. Experimental will be downloaded instead of Early Access.\\n"
-        STEAMBETAFLAG="experimental"
-    else
-        STEAMBETAFLAG="public"
-    fi
+if [ "$SKIPUPDATE_LOWER" != "true" ]; then
+  STEAMBETA_LOWER="$(to_lower "$STEAMBETA")"
+  if [ "$STEAMBETA_LOWER" = "true" ]; then
+    printf "Experimental flag is set. Experimental will be downloaded instead of Early Access.\\n"
+    STEAMBETAFLAG="experimental"
+  else
+    STEAMBETAFLAG="public"
+  fi
 
-    STORAGEAVAILABLE=$(stat -f -c "%a*%S" .)
-    STORAGEAVAILABLE=$((STORAGEAVAILABLE/1024/1024/1024))
-    printf "Checking available storage: %sGB detected\\n" "$STORAGEAVAILABLE"
+  STORAGEAVAILABLE="$(stat -f -c '%a*%S' .)"
+  STORAGEAVAILABLE="$((STORAGEAVAILABLE/1024/1024/1024))"
+  printf "Checking available storage: %sGB detected\\n" "$STORAGEAVAILABLE"
 
-    if [[ "$STORAGEAVAILABLE" -lt 8 ]]; then
-        printf "You have less than 8GB (%sGB detected) of available storage to download the game.\\nIf this is a fresh install, it will probably fail.\\n" "$STORAGEAVAILABLE"
-    fi
+  if [ "$STORAGEAVAILABLE" -lt 8 ]; then
+    printf "You have less than 8GB (%sGB detected) of available storage to download the game.\\n" "$STORAGEAVAILABLE"
+    printf "If this is a fresh install, it will probably fail.\\n"
+  fi
 
-    printf "\\nDownloading the latest version of the game...\\n"
-    if [ -f "/home/container/config/gamefiles/steamapps/appmanifest_1690800.acf" ]; then
-        printf "\\nRemoving the app manifest to force Steam to check for an update...\\n"
-        rm "/home/container/config/gamefiles/steamapps/appmanifest_1690800.acf" || true
-    fi
-    steamcmd +force_install_dir /home/container/config/gamefiles +login anonymous +app_update "$STEAMAPPID" -beta "$STEAMBETAFLAG" validate +quit
-    cp -r /home/container/.steam/steam/logs/* "/home/container/config/logs/steam" || printf "Failed to store Steam logs\\n"
+  printf "\\nDownloading the latest version of the game...\\n"
+  if [ -f "/home/container/config/gamefiles/steamapps/appmanifest_1690800.acf" ]; then
+    printf "\\nRemoving the app manifest to force Steam to check for an update...\\n"
+    rm "/home/container/config/gamefiles/steamapps/appmanifest_1690800.acf" || true
+  fi
+  steamcmd +force_install_dir /home/container/config/gamefiles \
+          +login anonymous \
+          +app_update "$STEAMAPPID" -beta "$STEAMBETAFLAG" validate +quit
+
+  cp -r /home/container/.steam/steam/logs/* "/home/container/config/logs/steam" \
+     || printf "Failed to store Steam logs\\n"
 else
-    printf "Skipping update as flag is set\\n"
+  printf "Skipping update as flag is set\\n"
 fi
 
+###############################################################################
+# Launch the game server
+###############################################################################
 printf "Launching game server\\n\\n"
 
 cp -r "/home/container/config/saved/server/." "/home/container/config/backups/"
-cp -r "${GAMESAVESDIR}/server/." "/home/container/config/backups" # useful after the first run
+cp -r "${GAMESAVESDIR}/server/." "/home/container/config/backups" 2>/dev/null || true
 rm -rf "$GAMESAVESDIR"
 ln -sf "/home/container/config/saved" "$GAMESAVESDIR"
 
 if [ ! -f "/home/container/config/gamefiles/FactoryServer.sh" ]; then
-    printf "FactoryServer launch script is missing.\\n"
-    exit 1
+  printf "FactoryServer launch script is missing.\\n"
+  exit 1
 fi
 
 cd /home/container/config/gamefiles || exit 1
 
-chmod +x FactoryServer.sh || true
+chmod +x FactoryServer.sh 2>/dev/null || true
 ./FactoryServer.sh -Port="$SERVERGAMEPORT" "${ini_args[@]}" "$@" &
 
 sleep 2
-satisfactory_pid=$(ps --ppid ${!} o pid=)
+satisfactory_pid="$(ps --ppid "$!" -o pid= 2>/dev/null)"
 
 shutdown() {
-    printf "\\nReceived SIGINT. Shutting down.\\n"
-    kill -INT $satisfactory_pid 2>/dev/null
+  printf "\\nReceived SIGINT or SIGTERM. Shutting down.\\n"
+  kill -INT "$satisfactory_pid" 2>/dev/null || true
 }
 trap shutdown SIGINT SIGTERM
 
