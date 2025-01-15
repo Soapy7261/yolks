302213da14d7fb38bb6c6fb09632029123e28835
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2025-01-14 21:46:09 -0500

    add mongodb 8 alpine

--- mongodb/entrypoint-alpine.sh
+++ mongodb/entrypoint-alpine.sh
@@ -1,22 +1,25 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 TZ=${TZ:-UTC}
 export TZ
 
+# Change to /home/container
 cd /home/container || exit 1
 
-# Make internal Docker IP address available to processes.
+# Make internal Docker IP address available to processes
 if command -v ip >/dev/null 2>&1; then
-    INTERNAL_IP=$(ip route get 1 | awk '{print $(NF-2);exit}')
+    INTERNAL_IP="$(ip route get 1 | awk '{print $(NF-2);exit}')"
     export INTERNAL_IP
 else
     echo 'IP not installed, skipping setting internal IP... (This should be ignored.)'
 fi
 
-# Run the Program
+# Create mongodb-data directory if it does not exist
 if [ ! -d "./mongodb-data" ]; then
     mkdir /home/container/mongodb-data
 fi
 
 PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+
+# Execute the resulting command via env
 exec env ${PARSED}
