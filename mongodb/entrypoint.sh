8e8d3f788b2cd218676289f5e0da1a9d6db2befa
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2025-01-17 15:40:15 -0500

    oops

--- mongodb/entrypoint.sh
+++ mongodb/entrypoint.sh
@@ -18,11 +18,13 @@ if [ ! -d "./mongodb-data" ]; then
     mkdir /home/container/mongodb-data
 fi
 
+PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
+exec env ${PARSED} & sleep 5
+
 if [ -d "./mongodb-data-to-restore" ]; then
     echo "Restoring MongoDB data..."
     mongorestore /home/container/mongodb-data-to-restore
     rm -rf /home/container/mongodb-data-to-restore
 fi
 
-PARSED=$(echo "${STARTUP}" | sed -e 's/{{/${/g' -e 's/}}/}/g' | eval echo "$(cat -)")
-exec env ${PARSED}
+wait
