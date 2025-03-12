575c0f3818b414b67a830802f46dcded5a058f41
Soapy7261 Official <81933267+Soapy7261@users.noreply.github.com>
2025-03-12 00:00:18 -0400

    replace with ENV at runtime

--- mclogs/entrypoint.sh
+++ mclogs/entrypoint.sh
@@ -7,4 +7,8 @@ fi
 echo 'Starting PHP FPM...'
 php-fpm -D
 echo 'Starting NGINX...'
+if [ -z "$SERVER_PORT" ]; then
+    SERVER_PORT=80
+fi
+sed -i "s/listen 80/listen ${SERVER_PORT}/g" /etc/nginx/conf.d/default.conf
 nginx -g 'daemon off;'
