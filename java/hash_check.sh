0744c3ff04a8db82244f4afe19d1677dd644eea6
Soapy7261 <81933267+Soapy7261@users.noreply.github.com>
2025-06-09 23:38:29 -0400

    graalvm 17 checking support

--- java/hash_check.sh
+++ java/hash_check.sh
@@ -10,8 +10,16 @@ fi
 version=$1
 
 if [ "${TARGETARCH}" = "amd64" ]; then
+    url_to_download="https://download.oracle.com/graalvm/${version}/latest/graalvm-jdk-${version}_linux-x64_bin.tar.gz"
+    if [ $2 = "17" ]; then
+        url_to_download="https://download.oracle.com/graalvm/17/archive/graalvm-jdk-17.0.12_linux-x64_bin.tar.gz.sha256"
+    fi
     expected_hash=$(curl -s "https://download.oracle.com/graalvm/${version}/latest/graalvm-jdk-${version}_linux-x64_bin.tar.gz.sha256")
 elif [ "${TARGETARCH}" = "arm64" ]; then
+    url_to_download="https://download.oracle.com/graalvm/${version}/latest/graalvm-jdk-${version}_linux-aarch64_bin.tar.gz.sha256"
+    if [ $2 = "17" ]; then
+        url_to_download="https://download.oracle.com/graalvm/17/archive/graalvm-jdk-17.0.12_linux-aarch64_bin.tar.gz.sha256"
+    fi
     expected_hash=$(curl -s "https://download.oracle.com/graalvm/${version}/latest/graalvm-jdk-${version}_linux-aarch64_bin.tar.gz.sha256")
 else
     echo "Unsupported architecture: ${TARGETARCH}"; exit 1;
